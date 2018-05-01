98acae23f1b2959f4770b3056a75e2db9fbd4abb
arieunier <arieunier@salesforce.com>
2018-05-01 18:22:13 +0200

    now uploading to s3

--- appsrc/photo.py
+++ appsrc/photo.py
@@ -4,9 +4,9 @@ from datetime import datetime
 import ujson
 import uuid
 from flask_bootstrap import Bootstrap
-from libs import postgres , utils , logs, rediscache
+from libs import postgres , utils , logs, rediscache, aws
 from appsrc import app, logger
-
+import time 
 
 RENDER_ROOT_PHOTO="photo_main.html"
 PATH_TO_TEST_IMAGES_DIR = './images'
@@ -14,15 +14,18 @@ PATH_TO_TEST_IMAGES_DIR = './images'
 @app.route('/photos_post', methods=['POST'])
 def image():
     try:
+        if (postgres.__checkHerokuLogsTable()):
+            postgres.__saveLogEntry(request)
         logger.debug(utils.get_debug_all(request))
-
-        import time 
-        print(request.files)
+        
         i = request.files['fileToUpload']  # get the image
         f = ('%s.jpeg' % time.strftime("%Y%m%d-%H%M%S"))
         i.save('%s/%s' % (PATH_TO_TEST_IMAGES_DIR, f))
-
-        return "Ok" , 200
+        completeFilename = '%s/%s' % (PATH_TO_TEST_IMAGES_DIR, f)
+        # now upload
+        logger.debug(completeFilename)
+        aws.uploadData(completeFilename)
+        return "File received, thanks for sharing.." , 200
     except Exception as e:
         import traceback
         traceback.print_exc()
--- libs/aws.py
+++ libs/aws.py
@@ -1,6 +1,6 @@
 import boto3
 import os 
-
+from appsrc import  logger
 
 BUCKETEER_AWS_ACCESS_KEY_ID=  os.getenv('BUCKETEER_AWS_ACCESS_KEY_ID','')
 BUCKETEER_AWS_REGION= os.getenv('BUCKETEER_AWS_REGION','')
@@ -9,17 +9,24 @@ BUCKETEER_BUCKET_NAME= os.getenv("BUCKETEER_BUCKET_NAME", "")
 
 
 
-print(BUCKETEER_AWS_ACCESS_KEY_ID)
-print(BUCKETEER_AWS_REGION)
-print(BUCKETEER_AWS_SECRET_ACCESS_KEY)
-print(BUCKETEER_BUCKET_NAME)
+logger.debug(BUCKETEER_AWS_ACCESS_KEY_ID)
+logger.debug(BUCKETEER_AWS_REGION)
+logger.debug(BUCKETEER_AWS_SECRET_ACCESS_KEY)
+logger.debug(BUCKETEER_BUCKET_NAME)
 
 
-boto3.set_stream_logger(name='botocore')
+#boto3.set_stream_logger(name='botocore')
 
 s3 = boto3.client('s3',aws_access_key_id=BUCKETEER_AWS_ACCESS_KEY_ID,aws_secret_access_key=BUCKETEER_AWS_SECRET_ACCESS_KEY)
-filename = 'image003.jpg'
-bucket_name = BUCKETEER_BUCKET_NAME
-s3.upload_file(filename, bucket_name, 'public/' + filename)
-url ="https://"+BUCKETEER_BUCKET_NAME + '.s3.' + BUCKETEER_AWS_REGION + '.amazonaws.com/' + 'public/' + filename
-print(url)
+
+def uploadData(filename):
+    try:
+        bucket_name = BUCKETEER_BUCKET_NAME
+        s3.upload_file(filename, bucket_name, 'public/' + filename)
+        url ="https://"+BUCKETEER_BUCKET_NAME + '.s3.' + BUCKETEER_AWS_REGION + '.amazonaws.com/' + 'public/' + filename
+        logger.debug(url)
+        return url
+    except Exception as e:
+        import traceback
+        traceback.print_exc()
+        return None
